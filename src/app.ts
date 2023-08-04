2dfad9ef7b4daf1060cb4890dcd0068d84ff13e3
yuu2dev <yuu2dev@gmail.com>
2023-08-04 20:51:01 +0900

    Build: 프로젝트 생성

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import { app, BrowserWindow } from 'electron'
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+    mainWindow.loadURL('/')
+})
+app.on('activate', () => {
+    const windows = BrowserWindow.getAllWindows()
+    if (!(windows && windows.length == 0)) {
+        return
+    }
+    /** @TODO 화면 다시 열기 */
+})
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
