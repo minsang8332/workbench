dfc6e3fd2b7f7d1ba55cc856fa03598e0ca3a488
yuu2dev <yuu2dev@gmail.com>
2024-02-04 14:03:59 +0900

    Chore: vuetify, scss 설치

--- webview-next/src/main.ts
+++ webview-next/src/main.ts
@@ -1,14 +1,19 @@
-import './assets/main.css'
-
+import '@/assets/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import App from '@/App'
+import router from '@/router'
+const vuetify = createVuetify({
+    components,
+    directives
+})
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
-
+app.use(vuetify)
 app.mount('#app')
--- webview-next/src/router/index.ts
+++ webview-next/src/router/index.ts
@@ -1,23 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history:
+        import.meta.env.NODE_ENV == 'production'
+            ? createWebHashHistory(import.meta.env.BASE_URL)
+            : createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            component: () => import('@/layouts/AppLayout'),
+            redirect: 'dashboard',
+            children: [
+                {
+                    name: 'dashboard',
+                    path: '/dashboard',
+                    component: () => import('@/views/DashboardPage')
+                }
+                /*
+                {
+                    name: 'markdown-editor',
+                    path: '/markdown-editor/:path',
+                    component: () => import('@/views/MdEditorPage.vue'),
+                    props: true
+                },
+                {
+                    name: 'account-book',
+                    path: '/account-book',
+                    component: () => import('@/views/AccountBookPage.vue')
+                }
+                */
+            ]
+        },
+        // notfound-404 핸들링
+        {
+            path: '/:pathMatch(.*)*',
+            redirect: 'dashboard'
+        }
+    ]
 })
-
 export default router
