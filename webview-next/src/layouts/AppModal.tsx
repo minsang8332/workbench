df878aec7b436e028b245c82e29bab8461714bb8
yuu2dev <yuu2dev@gmail.com>
2024-02-19 22:38:47 +0900

    fix: scss 폰트 URL 오기입 수정

--- webview-next/src/layouts/AppModal.tsx
+++ webview-next/src/layouts/AppModal.tsx
@@ -69,12 +69,12 @@ export default defineComponent({
                     <v-row class="am-row-actions" no-gutters>
                         <v-col>
                             <v-btn variant="text" block class="am-btn-no" onClick={hide}>
-                                취소
+                                <span>취소</span>
                             </v-btn>
                         </v-col>
                         <v-col>
                             <v-btn variant="text" block class="am-btn-ok" onClick={props.ok}>
-                                확인
+                                <span>확인</span>
                             </v-btn>
                         </v-col>
                     </v-row>
@@ -83,98 +83,3 @@ export default defineComponent({
         )
     }
 })
-/*
-<template>
-    <v-dialog
-        scrollable
-        content-class="app-modal"
-        :value="value"
-        @input="onInput"
-    >
-        <v-card class="card-app-modal fill-height" flat>
-            <v-row class="row-am-title bg-theme-g1" no-gutters>
-                <v-col align="center">
-                    <b class="text-am-title white--text">
-                        {{ title }}
-                    </b>
-                </v-col>
-            </v-row>
-            <v-divider />
-            <v-row class="row-am-message" no-gutters>
-                <v-col class="px-4">
-                    <p
-                        v-for="(message, i) in getMessage"
-                        class="text-am-message text-truncate"
-                        :key="`app-modal-msg-${i}`"
-                    >
-                        {{ message }}
-                    </p>
-                </v-col>
-            </v-row>
-            <template v-if="ok">
-                <v-row class="row-am-actions" no-gutters>
-                    <v-col>
-                        <v-btn
-                            text
-                            block
-                            class="btn-am-no"
-                            @click="onInput(false)"
-                            >취소</v-btn
-                        >
-                    </v-col>
-                    <v-col>
-                        <v-btn text block class="btn-am-ok" @click="ok"
-                            >확인</v-btn
-                        >
-                    </v-col>
-                </v-row>
-            </template>
-        </v-card>
-    </v-dialog>
-</template>
-<script>
-import _ from 'lodash'
-export default {
-    name: 'AppModal',
-    props: {
-        value: {
-            type: Boolean,
-            default: false,
-        },
-        title: {
-            type: [String, null],
-            default: '안내',
-        },
-        message: {
-            type: [String, Array, null],
-            default: '',
-        },
-        ok: {
-            type: [Function, null],
-            default: null,
-        },
-    },
-    computed: {
-        getMessage() {
-            let messages = []
-            try {
-                const message = this.message
-                if (_.isArray(message)) {
-                    messages = message
-                } else if (_.isString(message)) {
-                    messages.push(message)
-                }
-            } catch (e) {
-                console.error(e)
-            }
-            return messages
-        },
-    },
-    methods: {
-        onInput(value) {
-            this.$emit('input', value)
-        },
-    },
-}
-</script>
-*/
