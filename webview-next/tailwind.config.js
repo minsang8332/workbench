e858b9c628e60f606dab4e6fd3372987a7bf72e0
minsang8332 <minsang8332@gmail.com>
2024-11-16 11:42:10 +0900

    chore: tailwind-css 추가

---
+++ webview-next/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './src/**/*.{vue,js,ts,jsx,tsx,scss}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
